298a66310545ebaab9f462a84245ebb4b81ad2e2
Aavash Chhetri <68424695+A-atmos@users.noreply.github.com>
2023-10-06 22:19:13 +0545

    Prototype

---
+++ Assets/Scripts/OnPlayButtonClickEvent.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using Vuforia;
+
+public class Response
+{
+
+    public string result;
+
+
+    public static Response FromJson(string json)
+    {
+        return JsonUtility.FromJson<Response>(json);
+    }
+
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class OnPlayButtonClickEvent : MonoBehaviour
+{
+    public string devnagari_text_field;
+    public VirtualButtonBehaviour Vb;
+    private Response response;
+
+
+    void PostData() => StartCoroutine(GetAudioClip_Coroutine());
+
+    private void Start()
+    {
+        Vb.RegisterOnButtonPressed(ButtonTrigger);
+    }
+    public void ButtonTrigger(VirtualButtonBehaviour Vb)
+    {
+        Debug.Log("Button Presses");
+        PostData();
+    }
+
+    IEnumerator GetAudioClip_Coroutine()
+    {
+
+        string uri = "http://192.168.106.154:5000/";
+        string path = "";
+        string pathToAudio = "output";
+        string outputFromRequest = "";
+
+        WWWForm form = new WWWForm();
+        form.AddField("input_data", devnagari_text_field);
+
+        using (UnityWebRequest request = UnityWebRequest.Post(uri + path, form))
+        {
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                
+            }
+            else
+            {
+                outputFromRequest = request.downloadHandler.text;
+            }
+
+        }
+
+        Debug.Log(outputFromRequest);
+
+        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(uri+pathToAudio, AudioType.WAV))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                AudioClip myClip = DownloadHandlerAudioClip.GetContent(www);
+                AudioSource audioSource = GetComponent<AudioSource>();
+                audioSource.clip = myClip;
+                audioSource.Play();
+            }
+
+        }
+
+    }
+}
